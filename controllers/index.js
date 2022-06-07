faa3058f52662fa120a9fe2b8b6a537d1d1b4aaa
Sherreka <rekadarnb@gmail.com>
2022-06-07 19:35:22 -0400

    bar chart not working

--- controllers/index.js
+++ controllers/index.js
@@ -8,6 +8,14 @@ router.get("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err)
   }
+});
+
+router.get("/dashboard", async (req, res) => {
+  try {
+    res.render('dashboard', { body: 'test' })
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
   
