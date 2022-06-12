a2aebd5d07c301eae9cf26169fd5d1152242902b
Sherreka <rekadarnb@gmail.com>
2022-06-12 17:33:02 -0400

    changed route funcationality on front end

---
+++ public/js/generateInvoice.js
@@ -0,0 +1,6 @@
+const pdfArea = document.getElementById('display-pdf');
+const createBtn = document.getElementById('create-invoice');
+
+createBtn.addEventListener('click', () => {
+    console.log('It works');
+});
