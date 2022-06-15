8e31563444f73e9096bc4a8d13d3dc357a3cf781
Sherreka <rekadarnb@gmail.com>
2022-06-15 13:50:22 -0400

    fixed logout buttons on front end

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -63,4 +63,12 @@ router.post('/logout', (req, res) => {
   }
 });
 
+router.get('/logout', (req, res) => {
+  try {
+    res.render('login', { body: 'test' })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
 module.exports = router;
