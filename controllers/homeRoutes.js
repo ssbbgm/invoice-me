26633a5af890cba30639d54a4544e9bcefa9768f
Sherreka <rekadarnb@gmail.com>
2022-06-14 22:21:52 -0400

    removed forgot password and remember me toggle

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -57,7 +57,7 @@ router.get('/forgot', async (req, res) => {
     } catch (err) {
       res.status(500).json(err)
     }
-  });
+});
   
 router.get('/register', (req, res) => {
     try {
--- public/js/login.js
+++ public/js/login.js
@@ -2,9 +2,7 @@ const login = document.getElementById('login-form');
 
 const loginFormHandler = async (event) => {
     event.preventDefault();
-    console.log("testing");
     
-  
     // Collect values from the login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
