3ebb6a644afd77249920ac0cdb06b63ba21a3540
Mikhail25 <42049923+Mikhail25@users.noreply.github.com>
2022-06-14 22:15:34 -0400

    fixed login message method and changed form click to submit

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -21,18 +21,22 @@ router.post('/login', async (req, res) => {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      // res
+      //   .status(400)
+      //   .json({ message: 'Incorrect email or password, please try again' });
+      res.statusMessage = 'Incorrect email, please try again';
+      res.status(400).end();
       return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      // res
+      //   .status(400)
+      //   .json({ statusMessage: : 'Incorrect email or password, please try again' });
+      res.statusMessage = 'Incorrect password, please try again';
+      res.status(400).end();
       return;
     }
 
